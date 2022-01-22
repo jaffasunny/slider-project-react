d39567def3e4dbdb949b6e1e3e244118719462ad
jaffasunny <jaffer.sunny125@gmail.com>
2022-01-22 21:22:12 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
 	const [people, setPeople] = useState(data);
 	const [index, setIndex] = useState(0);
 
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, people]);
+
 	return (
 		<section className="section">
 			<div className="title">
@@ -22,6 +32,12 @@ function App() {
 					if (personIndex === index) {
 						position = "activeSlide";
 					}
+					if (
+						personIndex === index - 1 ||
+						(index === 0 && personIndex === people.length - 1)
+					) {
+						position = "lastSlide";
+					}
 
 					return (
 						<article className={position} key={id}>
@@ -33,10 +49,14 @@ function App() {
 						</article>
 					);
 				})}
-				<button className="prev">
+				<button
+					className="prev"
+					onClick={() => {
+						setIndex(index - 1);
+					}}>
 					<FiChevronLeft />
 				</button>
-				<button className="next">
+				<button className="next" onClick={() => setIndex(index + 1)}>
 					<FiChevronRight />
 				</button>
 			</div>
